7a5d2ac22e29a24b99c6426bc12175b5ba1df405
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-09 21:47:07 -0300

    feat(Main): Adiciona estilo condicional baseado no estado de autenticação
    Utiliza o hook `useGlobalState` para verificar a existência de um usuário logado.
    
    Com base no estado do usuário, a classe `mainLoggedIn` é adicionada dinamicamente ao contêiner principal, permitindo que o layout se adapte para usuários autenticados.

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,8 +1,16 @@
-import style from './Main.module.css';
-export function Main({children}: {children?: React.ReactNode}) {
-  return (
-    <main className={style["main"]}>
-     {children}
-    </main>
-  );
+import style from "./Main.module.css";
+import { useGlobalState } from "../../Hooks/useGlobalState";
+import type { ReactNode } from "react";
+
+interface MainProps {
+  children: ReactNode;
+}
+
+export function Main({ children }: MainProps) {
+  const { user } = useGlobalState();
+  const containerClasses = `${style.main} ${
+    user ? style.mainLoggedIn : ""
+  }`.trim();
+
+  return <div className={containerClasses}>{children}</div>;
 }
