024082b81d88a2f476cb348616615ace95b21940
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-09 21:59:58 -0300

    feat(pages): Cria a página inicial da aplicação
    Cria o componente da página Home, que serve como a tela de boas-vindas para os usuários.
    
    A página exibe uma mensagem de "Bem-vindo" e uma breve descrição sobre o propósito do aplicativo, direcionado ao controle de contas a receber para professores.

---
+++ src/Pages/Home/home.tsx
@@ -0,0 +1,21 @@
+import { Main } from "../../Components/Main/Main";
+import style from "./home.module.css";
+
+export function Home() {
+  return (
+    <div className={style["container"]}>
+      <Main>
+        <h1 className={style.title}>Bem-vindo</h1>
+
+        <p className={style.phrase}>
+          Um aplicativo simples e eficiente para o controle de contas a
+          receber.
+        </p>
+        <p className={style.phrase}>
+          Pensado para professores que desejam organizar seus recebimentos com
+          clareza e agilidade.
+        </p>
+      </Main>
+    </div>
+  );
+}
