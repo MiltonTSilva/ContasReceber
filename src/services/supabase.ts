528298a89573478443e09f70fe4ac8df4f952e39
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-09 22:14:52 -0300

    feat: Configura o cliente Supabase para interação com o backend
    Inicializa e exporta a instância do cliente Supabase, permitindo a comunicação
    com os serviços de autenticação, banco de dados e armazenamento do projeto.
    
    - Utiliza as variáveis de ambiente para o URL do projeto e a chave anon.
    - Prepara a base para operações de CRUD e autenticação seguras.

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-// É altamente recomendado usar variáveis de ambiente para estas chaves.
-// Substitua com a URL e a chave Anon do seu projeto Supabase.
-const supabaseUrl = "https://twsirteoilryvfmytblf.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3c2lydGVvaWxyeXZmbXl0YmxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQxNDAzMDgsImV4cCI6MjA1OTcxNjMwOH0.j1sa0cJq07E-Gh-KSru16-8rvdhL5_6p8XkCFkOZYG0";
+// Configuração do Supabase com variáveis de ambiente
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_URL_NONKEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+console.log("supabaseUrl :", import.meta.env.VITE_SUPABASE_URL);
+console.log("supabaseKey :", import.meta.env.VITE_SUPABASE_URL_NONKEY);
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Variáveis de ambiente VITE_SUPABASE_URL e VITE_SUPABASE_URL_NONKEY não estão definidas."
+  );
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
