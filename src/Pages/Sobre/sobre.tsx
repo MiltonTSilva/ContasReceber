35c854fc0481b0e491c85fbd4f918dc5f987b4ab
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-09 22:11:31 -0300

    feat(pages): Cria a página Sobre
    Adiciona a página "Sobre" que apresenta informações sobre o propósito do aplicativo e os dados de contato do desenvolvedor.
    
    - O componente `Sobre` é renderizado dentro do layout principal (`Main`).
    - A página contém detalhes de contato, incluindo e-mail, GitHub, LinkedIn e WhatsApp.

---
+++ src/Pages/Sobre/sobre.tsx
@@ -0,0 +1,39 @@
+import { Main } from "../../Components/Main/Main";
+import { useGlobalState } from "../../Hooks/useGlobalState";
+import style from "./sobre.module.css";
+
+export function Sobre() {
+  const { user } = useGlobalState();
+  if (!user) {
+    return <p>Você precisa estar logado para acessar esta página.</p>;
+  }
+  return (
+    <Main>
+      <h1 className={style.title}>Sobre</h1>
+      <p className={style.phrase}>
+        Este aplicativo foi criado com o objetivo de centralizar as contas a
+        receber de alunos.
+      </p>
+
+      <p className={style.phrase}>
+        O aplicativo é uma ferramenta simples e eficiente para o controle de
+        contas a receber, pensado especialmente para professores que desejam
+        organizar seus recebimentos com clareza e agilidade.
+      </p>
+
+      <address className={style.address}>
+        Criado por: Milton Tomé da Silva
+        <br />
+        E-mail: miltontsilva@gmail.com
+        <br />
+        <a href="https://github.com/MiltonTSilva">Github</a>
+        <br />
+        <a href="https://www.linkedin.com/in/miltontsilva/">LinkedIn</a>
+        <br />
+        <a href="https://api.whatsapp.com/send?phone=5511967079318">
+          Whatsapp: (11) 9.6707-9318
+        </a>
+      </address>
+    </Main>
+  );
+}
