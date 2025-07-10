cf92e199b9aa723cb02e8c8d0feff4ac6c26e9b8
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-09 22:01:01 -0300

    feat(auth): Cria a página de Login e a lógica de autenticação
    Este commit introduz o componente da página de Login, permitindo que os usuários acessem a aplicação com suas credenciais.
    
    A página inclui:
    - Um formulário com campos para email e senha.
    - Validação de campos obrigatórios.
    - Integração com o hook `useGlobalState` para chamar a função `signInWithPassword`.
    - Gerenciamento de estado para `loading` e `error`, desabilitando o botão durante a submissão e exibindo mensagens de erro.
    - Redirecionamento para a rota `/home` após um login bem-sucedido.
    - Um link para a página de cadastro (`/register`) para usuários que ainda não possuem uma conta.

---
+++ src/Pages/login/login.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Main } from "../../Components/Main/Main";
+import style from "./login.module.css";
+import { useGlobalState } from "../../Hooks/useGlobalState";
+import { Link, useNavigate } from "react-router-dom";
+
+export function Login() {
+  const navigate = useNavigate();
+  const { signInWithPassword, loading, error } = useGlobalState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const success = await signInWithPassword({ email, password });
+
+    if (success) {
+      navigate("/home");
+    }
+  };
+
+  return (
+    <Main>
+      <div className={style.container}>
+        <div className={style.card}>
+          <p className={style.title}>Contas a Receber</p>
+          <p className={style.subtitle}>Acesse sua conta para continuar.</p>
+
+          <form className={style["form"]} onSubmit={handleLogin}>
+            <p className={style.subtitle}>
+              Para fazer o login, utilize o email e senha cadastrados.
+            </p>
+
+            <input
+              className={style.input}
+              type="email"
+              placeholder="Digite seu email."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <input
+              className={style.input}
+              type="password"
+              placeholder="Digite sua senha."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <button className={style.button} type="submit" disabled={loading}>
+              {loading ? "Entrando..." : "Entrar"}
+            </button>
+            <hr className={style.separator} />
+            <Link to="/register">Não tem uma conta? Cadastre-se</Link>
+            {error && <p className={style.error}>{error}</p>}
+          </form>
+        </div>
+      </div>
+    </Main>
+  );
+}
