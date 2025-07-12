8e620856a6f137a0e4a56656a72afaa2d77e3b07
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-12 19:14:41 -0300

    feat(login): Adiciona foco automático no input de email e título
    - Adiciona o hook `useRef` para criar uma referência ao campo de input de email.
    - Utiliza o hook `useEffect` para focar automaticamente no campo de email assim que o componente é montado, melhorando a usabilidade da página de login.
    - Inclui um título "Login" para maior clareza e identificação da tela.

--- src/Pages/login/login.tsx
+++ src/Pages/login/login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Main } from "../../Components/Main/Main";
 import style from "./login.module.css";
 import { useGlobalState } from "../../Hooks/useGlobalState";
@@ -9,6 +9,7 @@ export function Login() {
   const { signInWithPassword, loading, error } = useGlobalState();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const nameInputRef = useRef<HTMLInputElement>(null);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -20,11 +21,17 @@ export function Login() {
     }
   };
 
+  useEffect(() => {
+    // Foca no input de nome quando o componente é montado
+    nameInputRef.current?.focus();
+  }, []);
+
   return (
     <Main>
       <div className={style.container}>
         <div className={style.card}>
           <p className={style.title}>Contas a Receber</p>
+          <p className={style.title}>Login</p>
           <p className={style.subtitle}>Acesse sua conta para continuar.</p>
 
           <form className={style["form"]} onSubmit={handleLogin}>
@@ -33,6 +40,7 @@ export function Login() {
             </p>
 
             <input
+              ref={nameInputRef}
               className={style.input}
               type="email"
               placeholder="Digite seu email."
