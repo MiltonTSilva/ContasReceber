783c23ae55d3a9743f383a610c457cf971479e87
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-11 23:27:52 -0300

    feat(dialogs): cria componente de diálogo de confirmação reutilizável
    Este commit introduz o novo componente `ConfirmationDialogs`.
    
    O objetivo deste componente é fornecer uma caixa de diálogo padronizada e reutilizável para solicitar a confirmação do usuário antes de executar uma ação crítica ou destrutiva, como a exclusão de um registro.
    
    Ele é construído sobre o componente genérico `Dialogs` e abstrai a criação dos botões "Confirmar" e "Cancelar", associando as funções `onConfirm` e `onClose` respectivamente. Isso simplifica seu uso em diferentes partes da aplicação, garantindo consistência visual e de comportamento.

---
+++ src/Components/Dialogs/ConfirmationDialogs/ConfirmationDialogs.tsx
@@ -0,0 +1,39 @@
+import { Button } from "../../Button/Button";
+import Dialogs from "../Dialogs/Dialogs";
+import style from "../Dialogs/Dialogs.module.css";
+
+interface ConfirmationDialogsProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  title: string;
+  message: string;
+}
+
+export function ConfirmationDialogs({
+  isOpen,
+  onClose,
+  onConfirm,
+  title,
+  message,
+}: ConfirmationDialogsProps) {
+  return (
+    <Dialogs
+      title={title}
+      isOpen={isOpen}
+      onClose={onClose}
+      footer={
+        <>
+          <Button variant="cancel" onClick={onClose}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={onConfirm}>
+            Confirmar
+          </Button>
+        </>
+      }
+    >
+      <p className={style.dialogsMessage}>{message}</p>
+    </Dialogs>
+  );
+}
