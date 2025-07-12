cd0c1573140f0e30d8f60e8379af27245fde242c
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-11 23:33:40 -0300

    feat(components): Cria o componente ErrorDialogs
    Este commit introduz o novo componente `ErrorDialogs`, uma especialização do componente genérico `Dialogs` para exibir mensagens de erro padronizadas na aplicação.
    
    O `ErrorDialogs` simplifica a exibição de alertas de erro ao encapsular a lógica de apresentação, recebendo as propriedades `isOpen`, `onClose`, `title` e `message`. Ele renderiza um diálogo com um único botão "OK" no rodapé, que aciona a função `onClose`, proporcionando uma experiência de usuário consistente para o tratamento de erros.

---
+++ src/Components/Dialogs/ErrorDialogs/ErrorDialogs.tsx
@@ -0,0 +1,32 @@
+import { Button } from "../../Button/Button";
+import Dialogs from "../Dialogs/Dialogs";
+import style from "../Dialogs.module.css";
+
+interface ErrorDialogsProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  message: string;
+}
+
+export function ErrorDialogs({
+  isOpen,
+  onClose,
+  title,
+  message,
+}: ErrorDialogsProps) {
+  return (
+    <Dialogs
+      title={title}
+      isOpen={isOpen}
+      onClose={onClose}
+      footer={
+        <Button variant="primary" onClick={onClose}>
+          OK
+        </Button>
+      }
+    >
+      <p className={style.dialogsMessage}>{message}</p>
+    </Dialogs>
+  );
+}
