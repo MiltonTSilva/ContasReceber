681c537e380b40c7859949d045fc2e4619af5401
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-14 19:44:00 -0300

    chore: Remove comentários

--- src/Types/GlobalTypes.ts
+++ src/Types/GlobalTypes.ts
@@ -6,12 +6,12 @@ import type {
 export type { SignInWithPasswordCredentials };
 
 export interface GlobalContextType {
-  user: User | null; // O usuário autenticado ou nulo
-  loading: boolean; // Estado de carregamento da sessão
-  error: string | null; // Mensagem de erro, se houver
-  clearError: () => void; // Função para limpar erros
-  signOut: () => Promise<void>; // Função para fazer logout
+  user: User | null;
+  loading: boolean; 
+  error: string | null; 
+  clearError: () => void; 
+  signOut: () => Promise<void>; 
   signInWithPassword: (
     credentials: SignInWithPasswordCredentials
-  ) => Promise<boolean>; // Função para fazer login
+  ) => Promise<boolean>; 
 }
