63e8cc28beb7d59d045e852ae1473d223451fda0
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-14 19:42:26 -0300

    feat: Adiciona display do nome usuário logado

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -14,72 +14,77 @@ export function NavBar() {
 
   return (
     user && (
-      <nav className={style.nav}>
-        <Link to="/home" className={style.brand}>
-          <div className={style.logo}>
-            <img src="/favicon.png" alt="icone" />
-            <span>Contas a Receber</span>
+      <>
+        <nav className={style.nav}>
+          <Link to="/home" className={style.brand}>
+            <div className={style.logo}>
+              <img src="/favicon.png" alt="icone" />
+              <span>Contas a Receber</span>
+            </div>
+          </Link>
+
+          <div className={style.menuIcon} onClick={toggleMenu}>
+            <div />
+            <div />
+            <div />
           </div>
-        </Link>
-
-        <div className={style.menuIcon} onClick={toggleMenu}>
-          <div />
-          <div />
-          <div />
-        </div>
-        <ul className={`${style.nav} ${menuOpen ? style.open : ""}`}>
-          <li>
-            <Link
-              className={style.link}
-              to="/Home"
-              onClick={() => setMenuOpen(false)}
-            >
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={style.link}
-              to="/clientes"
-              onClick={() => setMenuOpen(false)}
-            >
-              Clientes
-            </Link>
-          </li>
-          <li>
-            <Link
-              className={style.link}
-              to="/recebimento"
-              onClick={() => setMenuOpen(false)}
-            >
-              Recebimento
-            </Link>
-          </li>
-
-          <li>
-            <Link
-              className={style.link}
-              to="/sobre"
-              onClick={() => setMenuOpen(false)}
-            >
-              Sobre
-            </Link>
-          </li>
-          <li>
-            {user ? (
-              <Logout />
-            ) : (
+          <ul className={`${style.nav} ${menuOpen ? style.open : ""}`}>
+            <li>
               <Link
                 className={style.link}
-                to="/login"
+                to="/Home"
                 onClick={() => setMenuOpen(false)}
               >
-                Login
+                Home
               </Link>
-            )}
-          </li>
-        </ul>
-      </nav>
+            </li>
+            <li>
+              <Link
+                className={style.link}
+                to="/clientes"
+                onClick={() => setMenuOpen(false)}
+              >
+                Clientes
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={style.link}
+                to="/recebimento"
+                onClick={() => setMenuOpen(false)}
+              >
+                Recebimento
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                className={style.link}
+                to="/sobre"
+                onClick={() => setMenuOpen(false)}
+              >
+                Sobre
+              </Link>
+            </li>
+            <li>
+              {user ? (
+                <Logout />
+              ) : (
+                <Link
+                  className={style.link}
+                  to="/login"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Login
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+        <div className={style.user}>
+          <p>Usuário(a): {user?.user_metadata.full_name} </p>
+        </div>
+      </>
     )
   );
 }
