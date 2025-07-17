be86e7da1dac567b769e2d0ff735cdd6e7851ca1
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-16 23:40:59 -0300

    refactor: Altera nome da tabela

---
+++ src/Types/RecebimentosTypes.ts
@@ -0,0 +1,12 @@
+export type Recebimento = {
+  id: string;
+  received_date: string;
+  payment_received_at: string;
+  amount_to_receive: number;
+  costumer_id: string;
+  active: boolean;
+  custumer: {
+    id: string;
+    name: string;
+  } | null;
+};
