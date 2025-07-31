3554c1b4f645da3134966d93619805a9efa86f5e
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-31 15:17:52 -0300

    refactor: Adicionado espaço entre titulo e e direitos

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      © 2025 Contas a Receber. <span>Todos direitos reservados.</span>
+      © 2025 Contas a Receber.&nbsp;<span>Todos direitos reservados.</span>
     </footer>
   );
 }
