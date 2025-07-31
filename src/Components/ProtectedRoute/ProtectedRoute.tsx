526dc92be9c6f88dd44948c49e21860c93c3e111
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-31 15:23:49 -0300

    refactor: Removido comentários

--- src/Components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -10,12 +10,10 @@ export function ProtectedRoute({ children }: ProtectedRouteProps) {
   const { user, loading } = useGlobalState();
 
   if (loading) {
-    // Mostra uma mensagem de carregamento enquanto a sessão é verificada
     return <div>Carregando...</div>;
   }
 
   if (!user) {
-    // Se não houver usuário, redireciona para a página de login
     return <Navigate to="/login" replace />;
   }
 
--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,12 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Configuração do Supabase com variáveis de ambiente
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_URL_NONKEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error(
-    "Variáveis de ambiente VITE_SUPABASE_URL e VITE_SUPABASE_URL_NONKEY não estão definidas."
+    "Variáveis de ambiente VITE_SUPABASE_URL e VITE_SUPABASE_URL_NONKEY não estão definidas.",
   );
 }
 
