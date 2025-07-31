158896d898dd72d17073f6397a83709a0c28545c
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-07-31 15:35:21 -0300

    feat: Adiciona componente error dialog e translation para traduzir mensagens

--- src/Pages/Logout/logout.tsx
+++ src/Pages/Logout/logout.tsx
@@ -1,29 +1,71 @@
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useGlobalState } from "../../Hooks/useGlobalState";
 import style from "./logout.module.css";
 import { Button } from "../../Components/Button/Button";
+import { ErrorDialogs } from "../../Components/Dialogs/ErrorDialogs/ErrorDialogs";
+import { useGeminiTranslation } from "../../Hooks/useGeminiTranslation";
 
 export function Logout() {
   const { signOut } = useGlobalState();
   const navigate = useNavigate();
-
+  const [error, setError] = useState<string | null>(null);
   const handleLogout = async () => {
     try {
       await signOut();
-      navigate("/login"); // Redireciona para a página de login após sair
+      navigate("/login");
     } catch (error) {
-      console.error("Erro ao fazer logout:", error);
+      setError((error as Error).message);
     }
   };
 
+  const {
+    translate: geminiTranslate,
+    translatedText,
+    error: translationError,
+  } = useGeminiTranslation();
+
+  const translateError = useCallback(
+    (error: string) => {
+      geminiTranslate(error, "português do Brasil");
+      if (translationError) {
+        throw new Error("Erro na tradução: " + translationError);
+      }
+    },
+    [geminiTranslate, translationError]
+  );
+
+  useEffect(() => {
+    if (error) {
+      translateError(error);
+    }
+  }, [error, translateError]);
+
+  const handleCloseDialog = () => {
+    setError(null);
+    handleReturn();
+  };
+
+  const handleReturn = () => {
+    navigate("/home");
+  };
   return (
-    <Button
-      type="button"
-      variant="bg-danger"
-      className={style["logout-button"]}
-      onClick={handleLogout}
-    >
-      Sair
-    </Button>
+    <>
+      <Button
+        type="button"
+        variant="bg-danger"
+        className={style["logout-button"]}
+        onClick={handleLogout}
+      >
+        Sair
+      </Button>
+
+      <ErrorDialogs
+        title="Ocorreu um erro"
+        message={translatedText}
+        isOpen={error !== null && !!translatedText}
+        onClose={handleCloseDialog}
+      />
+    </>
   );
 }
