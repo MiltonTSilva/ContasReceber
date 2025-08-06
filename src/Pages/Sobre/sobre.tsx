cefb87434825ff06caaf929125b91bd9a17e7331
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-08-06 11:45:19 -0300

    refactor: Adiciona div com class container

--- src/Pages/Sobre/sobre.tsx
+++ src/Pages/Sobre/sobre.tsx
@@ -1,61 +1,63 @@
 import { Main } from "../../Components/Main/Main";
-import style from "./sobre.module.css";
+import styles from "./sobre.module.css";
 
 export function Sobre() {
   return (
     <Main>
-      <h1 className={style.title}>Sobre</h1>
-      <p className={style.phrase}>
-        Este aplicativo foi criado com o objetivo de centralizar as contas a
-        receber de clientes.
-      </p>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Sobre</h1>
+        <p className={styles.phrase}>
+          Este aplicativo foi criado com o objetivo de centralizar as contas a
+          receber de clientes.
+        </p>
 
-      <p className={style.phrase}>
-        O aplicativo é uma ferramenta simples e eficiente para o controle de
-        contas a receber, pensado para qualquer pessoa que deseja organizar seus
-        recebimentos com clareza e agilidade.
-      </p>
+        <p className={styles.phrase}>
+          O aplicativo é uma ferramenta simples e eficiente para o controle de
+          contas a receber, pensado para qualquer pessoa que deseja organizar
+          seus recebimentos com clareza e agilidade.
+        </p>
 
-      <address className={style.address}>
-        <span className={style.author}>Criado por:</span>
-        <span>Milton Tomé da Silva</span>
+        <address className={styles.address}>
+          <span className={styles.author}>Criado por:</span>
+          <span>Milton Tomé da Silva</span>
 
-        <span className={style.author}>E-mail:</span>
-        <span>miltontsilva@gmail.com</span>
+          <span className={styles.author}>E-mail:</span>
+          <span>miltontsilva@gmail.com</span>
 
-        <span className={style.author}>Github:</span>
-        <span>
-          <a
-            href="https://github.com/MiltonTSilva"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://github.com/MiltonTSilva
-          </a>
-        </span>
+          <span className={styles.author}>Github:</span>
+          <span>
+            <a
+              href="https://github.com/MiltonTSilva"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://github.com/MiltonTSilva
+            </a>
+          </span>
 
-        <span className={style.author}>LinkedIn:</span>
-        <span>
-          <a
-            href="https://www.linkedin.com/in/miltontsilva/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://www.linkedin.com/in/miltontsilva/
-          </a>
-        </span>
+          <span className={styles.author}>LinkedIn:</span>
+          <span>
+            <a
+              href="https://www.linkedin.com/in/miltontsilva/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              https://www.linkedin.com/in/miltontsilva/
+            </a>
+          </span>
 
-        <span className={style.author}>Whatsapp:</span>
-        <span>
-          <a
-            href="https://api.whatsapp.com/send?phone=5511967079318"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            (11) 9.6707-9318
-          </a>
-        </span>
-      </address>
+          <span className={styles.author}>Whatsapp:</span>
+          <span>
+            <a
+              href="https://api.whatsapp.com/send?phone=5511967079318"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              (11) 9.6707-9318
+            </a>
+          </span>
+        </address>
+      </div>
     </Main>
   );
 }
