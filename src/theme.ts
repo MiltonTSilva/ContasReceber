175d1170bb5d4bbefee904358d72f0586344c0e6
Milton Tomé da Silva <miltontsilva@gmail.com>
2025-08-06 11:28:06 -0300

    style: Melhoria continua de seletores

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,11 @@
 export const lightTheme = {
-  background: "#fff",
-  color: "#000",
+  background: "#F8F8FF",
+  color: "#121212",
 };
 
 export const darkTheme = {
   background: "#121212",
-  color: "#ffffff",
+  color: "#F8F8FF",
 };
 
 export type ThemeType = typeof lightTheme;
