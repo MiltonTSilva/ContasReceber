830e757ce0708a9a8b847ce46bd1c78fea0444f3
Milton Tomé da Silva <miltontsilva@gmail.com>
2026-02-06 14:27:42 -0300

    Routes

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -3,10 +3,34 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { Register } from "../Pages/login/resgister";
 import { Login } from "../Pages/login/login";
 import { Home } from "../Pages/Home/home";
+
 import { Clientes } from "../Pages/Clientes/clientes";
 import { ClientesForm } from "../Pages/Clientes/clientesForm";
+
+import { Empresas } from "../Pages/Empresas/empresas";
+import { EmpresasForm } from "../Pages/Empresas/empresasForm";
+
+import { Pagamentos } from "../Pages/Pagamentos/pagamentos";
+import { PagamentosForm } from "../Pages/Pagamentos/pagamentosForm";
+
 import { Recebimentos } from "../Pages/Recebimentos/recebimentos";
 import { RecebimentosForm } from "../Pages/Recebimentos/recebimentosForm";
+
+import { TiposDespesas } from "../Pages/TiposDespesas/tiposdepesas";
+import { TiposDespesasForm } from "../Pages/TiposDespesas/tiposdepesasForm";
+
+import { Despesas } from "../Pages/Despesas/depesas";
+import { DespesasForm } from "../Pages/Despesas/depesasForm";
+
+import { Usuarios } from "../Pages/Usuarios/usuarios";
+import { UsuariosForm } from "../Pages/Usuarios/usuariosForm";
+
+import { Vendas } from "../Pages/Vendas/vendas";
+import { VendasForm } from "../Pages/Vendas/vendasForm";
+
+import { Produtos } from "../Pages/Produtos/produtos";
+import { ProdutosForm } from "../Pages/Produtos/produtosForm";
+
 import { Sobre } from "../Pages/Sobre/sobre";
 
 import { ProtectedRoute } from "../Components/ProtectedRoute/ProtectedRoute";
@@ -23,6 +47,78 @@ export function AppRoutes() {
         element={
           <ProtectedRoute>
             <Sobre />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/despesas"
+        element={
+          <ProtectedRoute>
+            <Despesas />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/despesas/despesasForm"
+        element={
+          <ProtectedRoute>
+            <DespesasForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/despesas/despesasForm/:id"
+        element={
+          <ProtectedRoute>
+            <DespesasForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/empresas"
+        element={
+          <ProtectedRoute>
+            <Empresas />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/empresas/empresasForm"
+        element={
+          <ProtectedRoute>
+            <EmpresasForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/empresas/empresasForm/:id"
+        element={
+          <ProtectedRoute>
+            <EmpresasForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/usuarios"
+        element={
+          <ProtectedRoute>
+            <Usuarios />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/usuarios/usuariosForm"
+        element={
+          <ProtectedRoute>
+            <UsuariosForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/usuarios/usuariosForm/:id"
+        element={
+          <ProtectedRoute>
+            <UsuariosForm />
           </ProtectedRoute>
         }
       />
@@ -51,6 +147,30 @@ export function AppRoutes() {
         }
       />
       <Route
+        path="/pagamentos"
+        element={
+          <ProtectedRoute>
+            <Pagamentos />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/pagamentos/pagamentosForm"
+        element={
+          <ProtectedRoute>
+            <PagamentosForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/pagamentos/pagamentosForm/:id"
+        element={
+          <ProtectedRoute>
+            <PagamentosForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
         path="/recebimentos"
         element={
           <ProtectedRoute>
@@ -71,6 +191,80 @@ export function AppRoutes() {
         element={
           <ProtectedRoute>
             <RecebimentosForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/tiposdespesas"
+        element={
+          <ProtectedRoute>
+            <TiposDespesas />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/tiposdespesas/tiposdespesasForm"
+        element={
+          <ProtectedRoute>
+            <TiposDespesasForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/tiposdespesas/tiposdespesasForm/:id"
+        element={
+          <ProtectedRoute>
+            <TiposDespesasForm />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/vendas"
+        element={
+          <ProtectedRoute>
+            <Vendas />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/recebimentos/recebimentosForm"
+        element={
+          <ProtectedRoute>
+            <VendasForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/vendas/vendasForm/:id"
+        element={
+          <ProtectedRoute>
+            <VendasForm />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/produtos"
+        element={
+          <ProtectedRoute>
+            <Produtos />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/produtos/novo"
+        element={
+          <ProtectedRoute>
+            <ProdutosForm />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/produtos/:id"
+        element={
+          <ProtectedRoute>
+            <ProdutosForm />
           </ProtectedRoute>
         }
       />
